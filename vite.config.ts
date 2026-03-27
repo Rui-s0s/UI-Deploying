e7e3eb84b81180f0d441a9d8958d84ba6ac2d029
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-03-27 16:57:49 -0300

    fix: Bug vite no reinicia cuando cambia el css arreglado

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,22 @@ import { defineConfig } from 'vite'
 import devServer from '@hono/vite-dev-server'
 
 export default defineConfig({
-    plugins: [
-        devServer({
-            entry: 'src/index.tsx'
+  plugins: [
+    devServer({
+      entry: 'src/index.tsx',
+      injectClientScript: true
+    }),
+    // Custom plugin to watch the public folder and trigger a reload
+    {
+      name: 'watch-public',
+      configureServer(server) {
+        server.watcher.add('public')
+        server.watcher.on('change', (file) => {
+          if (/[\\/]public[\\/]/.test(file)) {
+            server.ws.send({ type: 'full-reload' })
+          }
         })
-    ]
+      }
+    }
+  ]
 })
