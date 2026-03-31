7f0120711b593e90ae5d8378c79194bcc9bb97eb
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-03-31 14:53:07 -0300

    chore: Cambio de dependencias para no usar tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import devServer from '@hono/vite-dev-server'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     devServer({
       entry: 'src/index.tsx',
       injectClientScript: true
